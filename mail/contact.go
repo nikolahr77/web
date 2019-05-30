40e1385dd52f5676819bf7227bf090b64caf05f0
Nikola Hristov <nikolahr0707@icloud.com>
2019-05-30 17:08:56 +0300

    formated code

--- mail/contact.go
+++ mail/contact.go
@@ -5,39 +5,38 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
 type Contact struct {
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Age int `json:"age"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Age     int    `json:"age"`
 	Address string `json:"address"`
 }
 
-func GetContact (w http.ResponseWriter,r *http.Request){
+func GetContact(w http.ResponseWriter, r *http.Request) {
 	var c Contact
 	json.NewDecoder(r.Body).Decode(&c)
 	w.Write([]byte("Getting contacts"))
 	fmt.Println(c)
 }
 
-func CreateContact(w http.ResponseWriter,r *http.Request){
+func CreateContact(w http.ResponseWriter, r *http.Request) {
 	var c Contact
 	json.NewDecoder(r.Body).Decode(&c)
 	w.Write([]byte("Adding contacts"))
 	fmt.Println(c)
 }
 
-func DeleteContact(w http.ResponseWriter,r *http.Request){
+func DeleteContact(w http.ResponseWriter, r *http.Request) {
 	var c Contact
 	json.NewDecoder(r.Body).Decode(&c)
 	w.Write([]byte("Deleting contacts"))
 	fmt.Println(c)
 }
 
-func EditContact(w http.ResponseWriter,r *http.Request){
+func EditContact(w http.ResponseWriter, r *http.Request) {
 	var c Contact
 	json.NewDecoder(r.Body).Decode(&c)
 	w.Write([]byte("Editing contacts"))
@@ -48,18 +47,17 @@ func EditContact(w http.ResponseWriter,r *http.Request){
 //
 //}
 
-func main(){
+func main() {
 	connStr := "user=postgres dbname=email sslmode=dissable password=1234"
-	db,err := sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
 	}
 
-	
 	r := mux.NewRouter()
-	r.HandleFunc("/contacts",GetContact).Methods("GET")
-	r.HandleFunc("/contacts",EditContact).Methods("PUT")
-	r.HandleFunc("/contacts",CreateContact).Methods("POST")
-	r.HandleFunc("/contacts",DeleteContact).Methods("DELETE")
-	http.ListenAndServe(":8080",r)
-	}
+	r.HandleFunc("/contacts", GetContact).Methods("GET")
+	r.HandleFunc("/contacts", EditContact).Methods("PUT")
+	r.HandleFunc("/contacts", CreateContact).Methods("POST")
+	r.HandleFunc("/contacts", DeleteContact).Methods("DELETE")
+	http.ListenAndServe(":8080", r)
+}
