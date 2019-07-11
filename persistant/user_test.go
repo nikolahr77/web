99fc0ca928ff7144108967fe3755c6bd34ba4dba
Nikola Hristov <nikolahr0707@icloud.com>
2019-07-11 15:48:37 +0300

    created tests for persistant/get user

--- persistant/user_test.go
+++ persistant/user_test.go
@@ -9,28 +9,30 @@ import (
 	"time"
 )
 
-func TestMessageRepository_Get(t *testing.T) {
+func TestUserRepository_Get(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer db.Close()
 
-	rows := sqlmock.NewRows([]string{"guid", "name", "content", "created_on", "updated_on"}).
-		AddRow("3ff-d2", "Welcome", "This is a welcome msg", time.Unix(10, 0).UTC(), time.Unix(10, 0).UTC())
+	rows := sqlmock.NewRows([]string{"guid", "name", "password", "email", "age", "created_on", "updated_on"}).
+		AddRow("3f2-fds-d2", "Ivo", "5lJm2Sy2dkv2uxX9FcrobuZCl8WnvZ6z7yLvlt3w.ps9HZLxZv2MK", "ivo@abv.bg", 35, time.Unix(10, 0).UTC(), time.Unix(10, 0).UTC())
 
-	mock.ExpectQuery("SELECT \\* FROM messages").
+	mock.ExpectQuery("SELECT \\* FROM users").
 		WithArgs("15").
 		WillReturnRows(rows)
 
-	myDB := persistant.NewMessageRepository(db)
+	myDB := persistant.NewUserRepository(db)
 
 	actual, err := myDB.Get("15")
 
-	expected := web.Message{
-		GUID:      "3ff-d2",
-		Name:      "Welcome",
-		Content:   "This is a welcome msg",
+	expected := web.User{
+		GUID:      "3f2-fds-d2",
+		Name:      "Ivo",
+		Password:  "5lJm2Sy2dkv2uxX9FcrobuZCl8WnvZ6z7yLvlt3w.ps9HZLxZv2MK",
+		Email:     "ivo@abv.bg",
+		Age:       35,
 		CreatedOn: time.Unix(10, 0).UTC(),
 		UpdatedOn: time.Unix(10, 0).UTC(),
 	}
@@ -38,22 +40,21 @@ func TestMessageRepository_Get(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestMessageRepositoryGetReturnQueryError(t *testing.T) {
+func TestUserRepositoryGetReturnQueryError(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer db.Close()
 
-	mock.ExpectQuery("SELECT \\* FROM messages").
-		WithArgs("15").
+	mock.ExpectQuery("SELECT \\* FROM users").
+		WithArgs("32ff-sad2-fg5").
 		WillReturnError(SQLerror{"SQL Error"})
 
-	myDB := persistant.NewMessageRepository(db)
+	myDB := persistant.NewUserRepository(db)
 
-	_, err = myDB.Get("15")
+	_, err = myDB.Get("32ff-sad2-fg5")
 	expectedError := SQLerror{"SQL Error"}
 
 	assert.Equal(t, expectedError, err)
-
 }
